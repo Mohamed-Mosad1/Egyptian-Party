c421dc182fbdeac259b5b8bcd7d8ab92aad0e4f5
Mohamed Mosad <mohamedmosaad115@gmail.com>
2023-11-04 22:37:53 +0200

    fix bugs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
+let isMenuOpen = false;
+let toggleBtn = $("#header .content .openNav");
+let toggleLinks = $("#leftMenu a");
+
 $(".slider").click(function () {
-  $(".innerFirst").not($(this).next()).slideUp(1000);
-  $(this).next().slideToggle(1000);
+  $(".innerFirst").not($(this).next()).slideUp(1000, "linear");
+  $(this).next().slideToggle(1000, "linear");
 });
 
-$("#header .content .openNav").click(function () {
-  $("#leftMenu").animate({ width: "250px" }, 50);
-  $("#header .content").animate({ marginLeft: "250px" }, 50);
+toggleBtn.click(function (e) {
+  if (isMenuOpen) {
+    $("#leftMenu").animate({ width: "0px" }, 50);
+    $("#header .content").animate({ marginLeft: "0px" }, 50);
+    isMenuOpen = false;
+  } else {
+    $("#leftMenu").animate({ width: "250px" }, 50);
+    $("#header .content").animate({ marginLeft: "250px" }, 50);
+    isMenuOpen = true;
+  }
+  e.stopPropagation();
 });
+
+$(document).click(function (e) {
+  if (!toggleBtn.is(e.target)) {
+    $("#leftMenu").animate({ width: "0px" }, 50);
+    $("#header .content").animate({ marginLeft: "0px" }, 50);
+  }
+  isMenuOpen = false;
+});
+
 $(".closebtn").click(function () {
+  $("#leftMenu").animate({ width: "0px" }, 50);
+  $("#header .content").animate({ marginLeft: "0px" }, 50);
+  isMenuOpen = false;
+});
+
+$("#leftMenu a[href^='#']").click(function (e) {
+  e.preventDefault();
+  let aHref = e.target.getAttribute("href");
+  let sectionOffset = $(aHref).offset().top;
+  $("html, body").animate({ scrollTop: sectionOffset }, "slow");
   $("#leftMenu").animate({ width: "0px" }, 50);
   $("#header .content").animate({ marginLeft: "0px" }, 50);
 });
 
-$("#leftMenu a[href^='#']").click(function (e) {
-  let aHref = e.target.getAttribute("href");
-  let sectionOffset = $(aHref).offset().top;
-  $("html,body").animate({ scrollTop: sectionOffset }, "slow");
-});
 let days = document.querySelector(".days");
 let hours = document.querySelector(".hours");
 let minutes = document.querySelector(".minutes");
@@ -43,11 +69,14 @@ function count() {
 
 count();
 
-$("textarea").keyup(function () {
+$("textarea").keydown(function (e) {
   let myLength = $(this).val().length;
-  $("#chars").text(100 - myLength);
   if (myLength >= 100) {
-    $("#chars").text("your available character finished");
-    $("#chars2").remove();
+    $("#chars").text("Your available characters have finished");
+    $("#chars2").addClass("d-none");
+    $(this).val($(this).val().substr(0, 100));
+  } else {
+    $("#chars").text(100 - myLength);
+    $("#chars2").removeClass("d-none");
   }
 });
